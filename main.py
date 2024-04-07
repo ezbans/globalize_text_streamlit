f79e7af6e790cb326566382ebaccc2ee61fc0987
Ezra Banoba <ezra@Ezras-MacBook-Pro.local>
2024-04-06 20:05:26 -0600

    Minor bug fix

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import streamlit as st
 from langchain import PromptTemplate
 from langchain.llms import OpenAI
-from dotenv import load_dotenv,find_dotenv
-load_dotenv(find_dotenv())
+from dotenv import load_dotenv
+load_dotenv()
 
 template = """
     Below is an email that may be poorly worded.
